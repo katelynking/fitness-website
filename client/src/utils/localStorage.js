17059f390aa2b11e61a4f80baf8df13272a530ee
Genevieve Nelson <GEN7232@gmail.com>
2022-07-29 10:45:18 -0400

    refactor book to exercises SERVER side

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -28,3 +28,34 @@ export const removeBookId = (bookId) => {
 
   return true;
 };
+
+export const getSavedExerciseIds = () => {
+  const savedExerciseIds = localStorage.getItem('saved_exercises')
+    ? JSON.parse(localStorage.getItem('saved_exercises'))
+    : [];
+
+  return savedExerciseIds;
+};
+
+export const saveExerciseIds = (exerciseIdArr) => {
+  if (exerciseIdArr.length) {
+    localStorage.setItem('saved_exercises', JSON.stringify(exerciseIdArr));
+  } else {
+    localStorage.removeItem('saved_exercises');
+  }
+};
+
+export const removeexerciseId = (exerciseId) => {
+  const savedExerciseIds = localStorage.getItem('saved_exercises')
+    ? JSON.parse(localStorage.getItem('saved_exercises'))
+    : null;
+
+  if (!savedExerciseIds) {
+    return false;
+  }
+
+  const updatedSavedExerciseIds = savedExerciseIds?.filter((savedExerciseId) => savedExerciseId !== exerciseId);
+  localStorage.setItem('saved_exercise', JSON.stringify(updatedSavedExerciseIds));
+
+  return true;
+};
