5109e226c3cd3260900a8895f0322d52e68ae1bb
Genevieve Nelson <GEN7232@gmail.com>
2022-08-01 15:09:13 -0400

    chat inital

--- client/src/pages/SavedExercises.js
+++ client/src/pages/SavedExercises.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
 // import { getMe, deleteExercise } from '../utils/API';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 // import { removeExerciseId } from '../utils/localStorage';
 // import { SearchExercises } from './SearchExercises';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_EXERCISE } from '../utils/mutations';
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_EXERCISE } from "../utils/mutations";
 
 const SavedExercises = () => {
   // const [userData, setUserData] = useState({});
@@ -29,6 +35,18 @@ const SavedExercises = () => {
     },
   });
 
+  const handleRemoveExercise = async (exerciseId) => {
+    try {
+      const { data } = await removeExercise({
+        variables: { exerciseId },
+      });
+      // console.log(`handleremove`);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const profile = data?.me || data?.profile || {};
 
   if (loading) {
@@ -44,83 +62,14 @@ const SavedExercises = () => {
     );
   }
 
-
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
-
-
-  // const handleDeleteExercise = async (exerciseId) => {
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     const response = await REMOVE_EXERCISE(exerciseId, token);
-
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
-
-  //     const updatedUser = await response.json();
-  //     setUserData(updatedUser);
-
-  //     removeExerciseId(exerciseId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-
-
-  const handleRemoveExercise = async (exerciseId) => {
-    try {
-      const { data } = await removeExercise({
-        variables: { exerciseId },
-      });
-      // console.log(`handleremove`);
-      console.log(data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   if (!profile.savedExercises.length) {
     return <h3>No Exercises Saved Yet</h3>;
   }
   console.log(profile);
-  // if data isn't here yet, say so
-  // if (!userDataLength) {
-  //   return <h2>LOADING...</h2>;
-  // }
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved exercisess!</h1>
         </Container>
@@ -128,17 +77,18 @@ const SavedExercises = () => {
       <Container>
         <h2>
           {profile.savedExercises.length
-            ? `Viewing ${profile.savedExercises.length} saved ${profile.savedExercises.length === 1 ? 'exercise' : 'exercises'}:`
-            : 'You have no saved exercises!'}
+            ? `Viewing ${profile.savedExercises.length} saved ${
+                profile.savedExercises.length === 1 ? "exercise" : "exercises"
+              }:`
+            : "You have no saved exercises!"}
         </h2>
         <CardColumns>
           {profile.savedExercises.map((exercise) => {
             return (
-              <Card key={exercise.exerciseId} border='dark'>
-
+              <Card key={exercise.exerciseId} border="dark">
                 <Card.Body>
                   <Card.Title>{exercise.name}</Card.Title>
-                  <p className='small'>Body Part: {exercise.bodyPart}</p>
+                  <p className="small">Body Part: {exercise.bodyPart}</p>
                   <Card.Text>{exercise.target}</Card.Text>
                   <p>Equipment: {exercise.equipment}</p>
                   <Card.Img
@@ -146,7 +96,10 @@ const SavedExercises = () => {
                     alt={`The cover for ${exercise.name}`}
                     variant="top"
                   />
-                  <Button className='btn-block btn-danger' onClick={() => handleRemoveExercise(exercise.exerciseId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleRemoveExercise(exercise.exerciseId)}
+                  >
                     Delete this Exercise!
                   </Button>
                 </Card.Body>
