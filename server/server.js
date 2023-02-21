fafc071152a138afbf41bcb3efdc55318e7b81c0
Katelyn King <katelynking112@gmail.com>
2023-02-21 10:13:16 -0500

    error when creating heroku app

--- server/server.js
+++ server/server.js
@@ -4,12 +4,6 @@ const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
-const serverio = require("http").createServer();
-const io = require("socket.io")(serverio, {
-  cors: {
-    origin: "*"
-  },
-});
 
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -22,8 +16,6 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -32,31 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-const chatPORT = 3002;
-const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-
-io.on("connection", (socket) => {
-  console.log(`Client ${socket.id} connected`);
-
-  // Join a conversation
-  const { roomId } = socket.handshake.query;
-  socket.join(roomId);
-
-  // Listen for new messages
-  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-  });
-
-  // Leave the room if the user closes the socket
-  socket.on("disconnect", () => {
-    console.log(`Client ${socket.id} diconnected`);
-    socket.leave(roomId);
-  });
-});
-
-serverio.listen(chatPORT, () => {
-  console.log(`Listening on port ${chatPORT}`);
-});
+// app.use(routes);
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -70,5 +38,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-
+// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
